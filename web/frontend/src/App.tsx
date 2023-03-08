6813e0db4ba906b7b0d864c1d464995806176c60
Njål Telstø <njal_telsto@hotmail.com>
2023-03-09 00:28:47 +0100

    Added mazemap markers

--- web/frontend/src/App.tsx
+++ web/frontend/src/App.tsx
@@ -1,60 +1,132 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import './App.css';
-import MapContainer from './components/MapConatiner';
-import SearchField from './components/SearchField';
-import UrlOverview from './components/UrlOverview';
-import { getRouterMap } from "./utils/api";
+
+type LongLat = {
+  lat: number,
+  lng: number,
+};
+
+type MapMarker = {
+  setLngLat: (lngLat: LongLat) => void,
+  addTo: (map: Map) => void,
+  remove: () => void,
+};
+
+type Poi = {
+  properties: {
+    id: number,
+  }
+};
+
+type MarkedPoi = {
+  poi: Poi,
+  marker: MapMarker,
+};
+
+type MapView = {
+  center: LongLat,
+  zoom: number,
+  speed?: number,
+};
+
+type Map = {
+  jumpTo: (view: MapView) => void,
+  on: (name: string, callback: (e: any) => void) => void,
+  zLevel: number,
+  highlighter: MapHighlighter,
+}
+
+type MapHighlighter = {
+  clear: () => void,
+  highlight: (poi: Poi) => void,
+};
 
 function App() {
-  const [URLs, setURLs] = useState<string[]>([]);
-  const [map, setMap] = useState<JSX.Element>();
-  const [mazemap, setMazemap] = useState<any>(undefined);
+  const [pois, setPois] = useState<MarkedPoi[]>([]);
+  const mapRef = useRef<Map>();
+  const poisRef = useRef<MarkedPoi[]>([]);
+
+  // Need to use a ref because mazemap object can't access
+  // useState variables directly
+  poisRef.current = pois;
 
   useEffect(() => {
     const script = document.createElement('script');
     script.src = 'https://api.mazemap.com/js/v2.0.9/mazemap.min.js'
-    script.addEventListener('load', () => {
-        const win = window as any;
-        setMazemap(new win.Mazemap.Map({container: 'mazemap-container'}));
-    })
-
-    const link = document.createElement('link');
-    link.rel = 'stylesheet';
-    link.href = 'https://api.mazemap.com/js/v2.0.9/mazemap.min.css';
+    script.addEventListener('load', initializeMap);
 
     document.body.appendChild(script);
-    document.body.appendChild(link);
     return () => {
       document.body.removeChild(script);
-      document.body.removeChild(link);
     }
   }, []);
 
-  function emptyURLs() {
-    setURLs([]);
+  function initializeMap() {
+    if (mapRef.current) return;
+
+    mapRef.current = new window.Mazemap.Map({container: 'mazemap-container'}) as Map;
+    mapRef.current.on('load', () => {
+      if (!mapRef.current) return;
+
+      mapRef.current.on('click', onMapClick);
+      mapRef.current.highlighter = new window.Mazemap.Highlighter(mapRef.current, {
+          showOutline: true,
+          showFill: true,
+          outlineColor: window.Mazemap.Util.Colors.MazeColors.MazeBlue,
+          fillColor: window.Mazemap.Util.Colors.MazeColors.MazeBlue
+      });
+
+      mapRef.current.jumpTo({
+        center: { lat: 63.41732271104283, lng: 10.404263674366945 },
+        zoom: 15.5,
+      });
+    })
   }
 
-  function addURL(url: string) {
-    setURLs((prevState: string[]) => {
-      return [...prevState, url];
+  function onMapClick(e: any) {
+    if (!mapRef.current) return;
+
+    const lngLat: LongLat = e.lngLat;
+    const zLevel: number = mapRef.current.zLevel;
+    if (!isNaN(zLevel)) {
+      window.Mazemap.Data.getPoiAt(lngLat, zLevel).then((poi: Poi) => {
+        const id = poi.properties.id;
+        if (poisRef.current.some((poi) => poi.poi.properties.id === id)) {
+          removePoid(id);
+        } else {
+          addPoi(poi);
+        }
+      })
+    }
+  }
+
+  function addPoi(poi: Poi) {
+    if (!mapRef.current) return;
+
+    const lngLat = window.Mazemap.Util.getPoiLngLat(poi);
+    var marker: MapMarker = new window.Mazemap.MazeMarker( {
+        zLevel: mapRef.current.zLevel,
+    });
+    marker.setLngLat(lngLat);
+    marker.addTo(mapRef.current);
+
+    setPois((prevState) => {
+      return [...prevState, { poi: poi, marker: marker }];
     });
   }
 
-  async function getMap() {
-    const map = await getRouterMap(URLs);
-    setMap(map);
+  function removePoid(poid: number) {
+    var poiToBeRemoved = poisRef.current.find((poi) => poi.poi.properties.id === poid);
+    if (poiToBeRemoved) {
+      poiToBeRemoved.marker.remove();
+      setPois((prevState) => {
+        return prevState.filter((poi) => poi.poi.properties.id !== poid);
+      });
+    }
   }
 
   return (
     <div className="App">
-      <div className="container">
-        <SearchField addURL={addURL} />
-        <MapContainer>
-          {map}
-        </MapContainer>
-        <button className="api-button" onClick={getMap}>Beregn plasseringer</button>
-      </div>
-      <UrlOverview URLs={URLs} emptyURLs={emptyURLs} />
       <div id='mazemap-container' style={{width: '500px', height: '500px'}} />
     </div>
   );
