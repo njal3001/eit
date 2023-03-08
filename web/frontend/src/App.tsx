44602056a4bbd3cf6ac2733fb6a6d8a68d4cd0c7
Njål Telstø <njal_telsto@hotmail.com>
2023-03-08 15:10:07 +0100

    Added mazemap container

--- web/frontend/src/App.tsx
+++ web/frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import './App.css';
 import MapContainer from './components/MapConatiner';
 import SearchField from './components/SearchField';
@@ -8,6 +8,27 @@ import { getRouterMap } from "./utils/api";
 function App() {
   const [URLs, setURLs] = useState<string[]>([]);
   const [map, setMap] = useState<JSX.Element>();
+  const [mazemap, setMazemap] = useState<any>(undefined);
+
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://api.mazemap.com/js/v2.0.9/mazemap.min.js'
+    script.addEventListener('load', () => {
+        const win = window as any;
+        setMazemap(new win.Mazemap.Map({container: 'mazemap-container'}));
+    })
+
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = 'https://api.mazemap.com/js/v2.0.9/mazemap.min.css';
+
+    document.body.appendChild(script);
+    document.body.appendChild(link);
+    return () => {
+      document.body.removeChild(script);
+      document.body.removeChild(link);
+    }
+  }, []);
 
   function emptyURLs() {
     setURLs([]);
@@ -34,6 +55,7 @@ function App() {
         <button className="api-button" onClick={getMap}>Beregn plasseringer</button>
       </div>
       <UrlOverview URLs={URLs} emptyURLs={emptyURLs} />
+      <div id='mazemap-container' style={{width: '500px', height: '500px'}} />
     </div>
   );
 }
