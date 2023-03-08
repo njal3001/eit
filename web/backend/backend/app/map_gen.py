3964ade2ab65a80974736b9a3e1031ddd1b0a2c2
sindrelothe <sindrl@online.no>
2023-03-08 12:53:19 +0100

    remove relative imports

--- web/backend/backend/app/map_gen.py
+++ web/backend/backend/app/map_gen.py
@@ -2,8 +2,8 @@ import requests
 import matplotlib.pyplot as plt
 import numpy as np
 from shapely import Point, Polygon, MultiPolygon
-from . import solver
-from .optimization import set_cover
+import solver
+from optimization import set_cover
 
 U1_URL = 'https://api.mazemap.com/api/pois/closestpoi/?lat=63.41588046866937&lng=10.405878134312957&z=-1&srid=4326'
 GROUP_ROOM1_URL = 'https://api.mazemap.com/api/pois/closestpoi/?lat=63.4157625337231&lng=10.40580125094317&z=-1&srid=4326'
@@ -218,7 +218,7 @@ def main():
     if full_polygon.geom_type == 'Polygon':
         full_polygon = MultiPolygon([full_polygon])
 
-    grid = create_bounding_grid(all_points, 2.0)
+    grid = create_bounding_grid(all_points, 3.0)
     valid_grid = list(filter(full_polygon.contains, grid))
 
     colors = ["red", "blue", "yellow", "orange"]
@@ -254,5 +254,5 @@ def main():
 
     plt.show()
 
-# if __name__ == '__main__':
-    # main()
+if __name__ == '__main__':
+    main()
