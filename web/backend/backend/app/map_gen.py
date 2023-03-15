a2ff9a473397e87d1842d221b9d5a71ef84061f2
Njål Telstø <njal_telsto@hotmail.com>
2023-03-15 11:31:49 +0100

    Fixed wrong imports

--- web/backend/backend/app/map_gen.py
+++ web/backend/backend/app/map_gen.py
@@ -2,8 +2,8 @@ import requests
 import matplotlib.pyplot as plt
 import numpy as np
 from shapely import Point, Polygon, MultiPolygon
-import solver
-from optimization import set_cover
+from . import solver
+from .optimization import set_cover
 import pickle
 
 U1_URL = 'https://api.mazemap.com/api/pois/closestpoi/?lat=63.41588046866937&lng=10.405878134312957&z=-1&srid=4326'
@@ -145,70 +145,6 @@ def create_bounding_grid(points, resolution):
     max_p = Point(max_x, max_y)
 
     return create_rectangular_grid(min_p, max_p, resolution)
-
-
-def draw_solution(urls):
-    rooms = []
-    for url in urls:
-        rooms.append(fetch_room(url))
-
-    coordinate_origin = rooms[0].origin
-    all_points = []
-    polygons = []
-    for r in rooms:
-        points = coordinates_to_origin_points(coordinate_origin, r.coordinates)
-        hole_points = []
-        for h in r.holes:
-            hole_points.append(coordinates_to_origin_points(coordinate_origin, h))
-
-        all_points += points
-
-        # Add a buffer around Polygon to reduce distance between duplicate walls
-        polygons.append(Polygon(points, holes=hole_points).buffer(solver.WALL_TOLERANCE))
-
-    # Merge rooms into single MultiPolygon
-    full_polygon = polygons[0]
-    for p in polygons[1:]:
-        full_polygon = full_polygon.union(p)
-
-    if full_polygon.geom_type == 'Polygon':
-        full_polygon = MultiPolygon([full_polygon])
-
-    grid = create_bounding_grid(all_points, 2.0)
-    valid_grid = list(filter(full_polygon.contains, grid))
-
-    colors = ["red", "blue", "yellow", "orange"]
-    color_index = 0
-    for geom in full_polygon.geoms:
-        xe, ye = geom.exterior.xy
-        plt.plot(xe, ye, color=colors[color_index], alpha=0.7)
-
-        for interior in geom.interiors:
-            xi, yi = zip(*interior.coords[:])
-            plt.plot(xi, yi, color=colors[color_index], alpha=0.7)
-
-        color_index = (color_index + 1) % len(colors)
-
-
-    for p in valid_grid:
-        plt.plot(p.x, p.y, 'o', ms=1, color='black')
-
-    covers = solver.solve(valid_grid, full_polygon)
-    res = set_cover(np.array(covers))
-    for i in range(len(res.x)):
-        if res.x[i] == 1:
-            cover = covers[i]
-            for j in range(len(cover)):
-                if cover[j] == 1:
-                    p1 = valid_grid[j]
-                    plt.plot(p1.x, p1.y, "o", ms=2, color=colors[color_index], alpha=0.7)
-
-            p = valid_grid[i]
-            plt.plot(p.x, p.y, 'o', ms=5, color='green')
-
-            color_index = (color_index + 1) % len(colors)
-
-    return plt.gcf()
 
 def get_router_coverage_map(poids):
     plt.clf()
